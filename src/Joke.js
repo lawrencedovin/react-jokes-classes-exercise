9cc98efa210766f56f6ce8295d0f77787cd8e5bf
Lawrence Dovin <lawrencedovin@gmail.com>
2021-07-22 17:24:08 -0400

    Updated Joke file to use class component instead of hooks

--- src/Joke.js
+++ src/Joke.js
@@ -1,27 +1,69 @@
 import React from "react";
 import "./Joke.css";
 
-function Joke({ vote, votes, text, id }) {
-  const upVote = () => vote(id, +1);
-  const downVote = () => vote(id, -1);
+class Joke extends React.Component{
 
-  return (
-    <div className="Joke">
-      <div className="Joke-votearea">
-        <button onClick={upVote}>
-          <i className="fas fa-thumbs-up" />
-        </button>
+  state = { votes: 0 }
 
-        <button onClick={downVote}>
-          <i className="fas fa-thumbs-down" />
-        </button>
+  upVote = () => {
+    this.setState({votes: this.state.votes + 1});
+  }
 
-        {votes}
+  downVote = () => {
+    this.setState({votes: this.state.votes - 1});
+  }
+
+  render() {
+    const {vote, text, id} = this.props;
+    const {votes} = this.state;
+
+    return (
+      <div className="Joke">
+        <div className="Joke-votearea">
+          <button onClick={this.upVote}>
+            <i className="fas fa-thumbs-up" />
+          </button>
+
+          <button onClick={this.downVote}>
+            <i className="fas fa-thumbs-down" />
+          </button>
+          {votes}
+        </div>
+
+        <div className="Joke-text">{text}</div>
       </div>
-
-      <div className="Joke-text">{text}</div>
-    </div>
-  );
+    )
+  }
 }
 
 export default Joke;
+
+
+
+// import React from "react";
+// import "./Joke.css";
+
+// function Joke({ vote, votes, text, id }) {
+//   const upVote = () => vote(id, +1);
+//   const downVote = () => vote(id, -1);
+
+//   return (
+//     <div className="Joke">
+//       <div className="Joke-votearea">
+//         <button onClick={upVote}>
+//           <i className="fas fa-thumbs-up" />
+//         </button>
+
+//         <button onClick={downVote}>
+//           <i className="fas fa-thumbs-down" />
+//         </button>
+
+//         {votes}
+//       </div>
+
+//       <div className="Joke-text">{text}</div>
+//     </div>
+//   );
+// }
+
+// export default Joke;
